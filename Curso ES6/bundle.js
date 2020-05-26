b1b83e401394c37a053a7c7f0d352ec18e9b4735
pedrohenriquebraga <gamesantos7@gmail.com>
2020-05-26 16:25:38 -0300

    use class

---
+++ Curso ES6/bundle.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function teste(x) {
+  var y = 4;
+
+  if (x > 5) {
+    console.log(x, y);
+  }
+}
+
+teste(10);
---
+++ Curso ES6/main.js
@@ -0,0 +1,9 @@
+function teste(x) {
+    let y = 4
+
+    if (x > 5) {
+        console.log(x, y)
+    }
+}
+
+teste(10)
